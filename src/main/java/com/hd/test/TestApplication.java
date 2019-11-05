e8fd5c07c569de38fb9be16bb0c02f098576f31b
houdu <hd@epshealth.com>
2019-11-05 17:56:48 +0800

    update yml config

--- src/main/java/com/hd/test/TestApplication.java
+++ src/main/java/com/hd/test/TestApplication.java
@@ -1,8 +1,12 @@
 package com.hd.test;
 
 import com.hd.test.common.Log4jUtils;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileNotFoundException;
 
@@ -12,6 +16,19 @@ public class TestApplication {
     public static void main(String[] args) throws FileNotFoundException {
         Log4jUtils.setLogConfigFilePath();// 加载日志配置
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    /**
+     * 加载YML格式自定义配置文件
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+//        yaml.setResources(new FileSystemResource("/usr/properties/test.yml"));//File引入
+		yaml.setResources(new ClassPathResource("test.yml"));//class引入
+        configurer.setProperties(yaml.getObject());
+        return configurer;
     }
 
 }
