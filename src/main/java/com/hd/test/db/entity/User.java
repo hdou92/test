6c96a616dd7020776c3bca8dbb645ece71f37973
houdu <hd@epshealth.com>
2020-02-18 10:09:05 +0800

    update user entity add get/set

--- src/main/java/com/hd/test/db/entity/User.java
+++ src/main/java/com/hd/test/db/entity/User.java
@@ -2,19 +2,58 @@ package com.hd.test.db.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import lombok.ToString;
 
 /**
  * 用户类
- * @Data 编译生成get/set方法
  * @TableName 声明表名 不配置 mybatis plus 则获取表名默认按实体名称驼峰获取表名  如： TestUser = test_user
  */
-@Data
 @TableName("user")
-@ToString
+@Data
 public class User {
     private Long id;
     private String name;
     private Integer age;
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
